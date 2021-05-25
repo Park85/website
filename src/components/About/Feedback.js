1abe7f2c6f1e589c62e587606cefa18ef9785185
saby22 <sabyasachi.tffs@gmail.com>
2021-05-25 10:02:58 +0530

    Reduced padding for feedback section in mobile devices

--- src/components/About/Feedback.js
+++ src/components/About/Feedback.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Form from '../common/Form';
 import * as styles from './feedback.module.scss';
 const Feedback = () => {
-  return (
-    <section style={{ backgroundColor: '#f7f7f7', width: '100%', padding: '5em 2em' }}>
-      <div className={styles.formContainer}>
-        <h1 className={styles.header}>Give us your valuable feedback</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, error.</p>
-        <div className={styles.formWrapper}>
-          <Form />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.feedbackSection}>
+			<div className={styles.formContainer}>
+				<h1 className={styles.header}>Give us your valuable feedback</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+					error.
+				</p>
+				<div className={styles.formWrapper}>
+					<Form />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Feedback;
